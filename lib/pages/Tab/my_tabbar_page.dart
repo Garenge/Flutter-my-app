698bc920bf44c2f165bc11cd74006320521a1794
ex_liuzp9 <ex_liuzp9@partner.midea.com>
2026-02-07 10:17:17 +0800

    tabbardemo, 添加自定义导航

--- lib/pages/Tab/my_tabbar_page.dart
+++ lib/pages/Tab/my_tabbar_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:my_app/widgets/unified_page_scaffold.dart';
 
 class MyTabbarPage extends StatefulWidget {
   const MyTabbarPage({super.key});
@@ -9,6 +10,7 @@ class MyTabbarPage extends StatefulWidget {
 
 class _MyTabbarPageState extends State<MyTabbarPage> {
   int _currentIndex = 0;
+  Color _appBarBackgroundColor = Colors.yellow;
 
   final List<Widget> _pages = [
     Container(color: Colors.red),
@@ -16,15 +18,34 @@ class _MyTabbarPageState extends State<MyTabbarPage> {
     Container(color: Colors.green),
   ];
 
+  Color _getAppBarBackgroundColor(int index) {
+    return [Colors.yellow, Colors.red, Colors.blue][index];
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: UnifiedAppBar(
+        title: const Text(
+          'Tabbar',
+          style: TextStyle(
+            fontSize: 18,
+            fontWeight: FontWeight.w600,
+            color: Colors.black,
+            inherit: false,
+          ),
+        ),
+        backgroundColor: _appBarBackgroundColor,
+      ),
       body: _pages[_currentIndex],
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: _currentIndex,
         onTap: (index) {
           setState(() {
             _currentIndex = index;
+            print('index: $index');
+            _appBarBackgroundColor = _getAppBarBackgroundColor(index);
+            print('appBarBackgroundColor: $_appBarBackgroundColor');
           });
         },
         items: const [
